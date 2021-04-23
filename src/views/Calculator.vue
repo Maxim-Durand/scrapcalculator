5db8aac4499664ab8cf04dd9a3b907832cf0ce31
Maxim Durand <durandmaxime1931@gmail.com>
2021-04-23 20:25:37 +0200

    First modification to see if TravisCI is doing its job

---
+++ src/views/Calculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      This is the first page of our scrap Calculator for Rust.
+    </h1>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+
+  },
+})
+export default class Calculator extends Vue {}
+</script>
